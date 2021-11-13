6effef52dfb38ecd1f4d589950b6a259e10f66ba
Serhii Plastun <scoced@gmail.com>
2021-11-13 16:00:12 +0200

    Validator

---
+++ task1-1Validator.js
@@ -0,0 +1,20 @@
+function Validator() {
+    function validateEmail(toValidate) {
+        return (/^[^\s\d\.-]([a-zA-Z\-\.\+\d]{1,20})@([a-zA-Z\d\’\+\?\^\&\.\*\!\/\=\$\%\_\-]{1,15})\.([a-zA-Z]{1,5})$/g).test(toValidate);
+    }
+
+    function validatePhone(toValidate) {
+        const regExp = /\d\-\(\)\s/;
+        if (toValidate.length > 25 || toValidate.length < 10) {
+            return false;
+        }
+        if (!toValidate.test(regExp)) {
+            return false;
+        }
+        return true;
+    }
+
+    function validatePassword(toValidate) {
+        return (/(?=.*[A-Z][a-z][0-9]).{8,}/).test(toValidate);
+    }
+}
